2910f52249ce81125a5d7bb8ce2f6bab899f1368
galeanolle <galeanolle@gmail.com>
2019-06-05 01:29:37 -0300

    - deprecated camera y compat install with npm

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -44,36 +44,6 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
-        "id": "cordova-plugin-camera.Camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "Camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
-        "id": "cordova-plugin-camera.CameraPopoverOptions",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "CameraPopoverOptions"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/www/Camera.js",
-        "id": "cordova-plugin-camera.camera",
-        "pluginId": "cordova-plugin-camera",
-        "clobbers": [
-            "navigator.camera"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-camera/src/browser/CameraProxy.js",
-        "id": "cordova-plugin-camera.CameraProxy",
-        "pluginId": "cordova-plugin-camera",
-        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -82,8 +52,7 @@ module.exports.metadata =
     "phonegap-plugin-barcodescanner": "8.0.1",
     "cordova-plugin-inappbrowser": "3.0.0",
     "cordova-plugin-dialogs": "2.0.1",
-    "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-camera": "4.0.3"
+    "cordova-plugin-whitelist": "1.3.3"
 }
 // BOTTOM OF METADATA
 });
